b0b682ec9fc81159e5f566ac87607dac961434fe
Madison Schultz <167554381+madischu@users.noreply.github.com>
2026-04-01 19:31:23 -0400

    Refactor main.cpp to accept user input for large integers

--- main.cpp
+++ main.cpp
@@ -4,24 +4,39 @@ using namespace std;
 
 int main()
 {
-    largeIntegers A("123456789987654321");
-    largeIntegers B("987654321123456789");
+    string input 1, input2;
+    // Prompt user for input
+    cout << "Enter first large integer: ";
+    cin >> input 1;
 
+    cout << "Enter seconf large integer: ";
+    cin >> input2;
+    
+    // Create objects using user input
+    largeIntegers A(input1);
+    largeIntegers B(input2);
+
+    // Perform operations
     largeIntegers sum = A.add(B);
     largeIntegers diff = A.subtract(B);
     largeIntegers prod = A.multiply(B);
 
-    cout << "A = "; A.print(); cout << endl;
+    // Output results
+    cout << "\nA = "; A.print(); cout << endl;
     cout << "B = "; B.print(); cout << endl;
 
     cout << "A + B = "; sum.print(); cout << endl;
     cout << "A - B = "; diff.print(); cout << endl;
     cout << "A * B = "; prod.print(); cout << endl;
 
+    // Comparison
     int cmp = A.compare(B);
-    if (cmp == 0) cout << "A == B" << endl;
-    else if (cmp > 0) cout << "A > B" << endl;
-    else cout << "A < B" << endl;
+    if (cmp == 0) 
+        cout << "A == B" << endl;
+    else if (cmp > 0) 
+        cout << "A > B" << endl;
+    else 
+        cout << "A < B" << endl;
 
     return 0;
 }
